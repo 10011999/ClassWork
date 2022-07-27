7d7121bb14d51725708a3ea1234b6f577be75bc8
10011999 <motewaraditya77@gmail.com>
2022-07-27 11:58:41 +0530

    Update:Exception

---
+++ com/bridgelabz/InputForm.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class InputForm {
+    public static void main(String[] args) {
+
+
+        InputForm inputForm = new InputForm();
+        try {
+            inputForm.findInput("");
+        } catch (InputNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void findInput(String input) throws InputNotFoundException {
+        if ("".equals(input)) {
+            throw new InputNotFoundException("Input is empty!");
+        }
+    }
+    }
+
+
+
+
+
---
+++ com/bridgelabz/InputNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class InputNotFoundException extends Exception {
+    public InputNotFoundException(String message) {
+        super(message);
+    }
+}
