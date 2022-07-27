67e8cce67aaac302480da4d3ca42d8f275728ebc
10011999 <motewaraditya77@gmail.com>
2022-07-27 11:17:09 +0530

    Update:stream n num added

---
+++ SumOfFirstNnum.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumOfFirstNnum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the N Value");
+        int n = in.nextInt();
+        ArrayList<Integer> integers = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            integers.add(i);
+        }
+        Integer ans = integers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(ans);
+    }
+}
