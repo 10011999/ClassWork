d184ba1d4b087c75c314a09fd8d5ae13c6fd52b0
10011999 <motewaraditya77@gmail.com>
2022-07-27 11:13:29 +0530

    Modified:Travers Array

---
+++ TraversArray.java
@@ -0,0 +1,9 @@
+public class TraversArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int i,a;
+        for (i=0;i< arr.length;i++){
+            System.out.println(i);
+        }
+    }
+}
